bb17620577e0162b7b01a3187e876c12650b9eb2
ebouther <ebouther@student.42.fr>
2017-08-19 15:32:31 +0200

    forgot new folders

---
+++ ft_nm/inc/nm.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nm.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/15 15:22:44 by ebouther          #+#    #+#             */
+/*   Updated: 2017/06/07 18:15:29 by ebouther         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NM_H
+# define NM_H
+
+# include <stdio.h>
+# include <sys/mman.h>
+# include <mach-o/loader.h>
+# include <mach-o/nlist.h>
+# include <mach-o/fat.h>
+# include <mach-o/ranlib.h>
+# include <ar.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <unistd.h>
+
+#                          include <stdlib.h> // remove norme
+#                          include <string.h>
+# include "libft.h"
+
+typedef struct 		s_sym
+{
+	uint8_t			arch_64;
+	uint64_t		value;
+	char			type;
+	uint8_t			n_sect;
+	char			*name;
+	struct s_sym	*next;
+}					t_sym;
+
+typedef struct 		s_sect
+{
+	char			section;
+	struct s_sect	*next;
+}					t_sect;
+
+void nm(char *f, char *ptr);
+
+#endif
---
+++ ft_nm/src/swap.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebouther <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/07 17:32:57 by ebouther          #+#    #+#             */
+/*   Updated: 2017/08/05 20:09:00 by ebouther         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "swap.h"
+
+uint32_t	swap_uint32(uint32_t x)
+{
+	return (((x & 0xff000000) >> 24) | 
+			((x & 0x00ff0000) >>  8) | 
+			((x & 0x0000ff00) <<  8) | 
+			((x & 0x000000ff) << 24));
+}
+
+void swap_fat_header(struct fat_header *fat_header, uint8_t l_endian)
+{
+	if (!l_endian)
+		return ;
+	fat_header->magic = swap_uint32(fat_header->magic);
+	fat_header->nfat_arch = swap_uint32(fat_header->nfat_arch);
+}
+
+void swap_load_command(struct load_command *lc, uint8_t l_endian)
+{
+	if (!l_endian)
+		return ;
+	lc->cmd     = swap_uint32(lc->cmd);
+	lc->cmdsize = swap_uint32(lc->cmdsize);
+}
+
+void swap_fat_arch(struct fat_arch *fat_archs, uint32_t nfat_arch, uint8_t l_endian)
+{
+	uint32_t i;
+
+	if (!l_endian)
+		return ;
+	i = 0;
+	while (i < nfat_arch)
+	{
+		fat_archs[i].cputype    = swap_uint32(fat_archs[i].cputype);
+		fat_archs[i].cpusubtype = swap_uint32(fat_archs[i].cpusubtype);
+		fat_archs[i].offset     = swap_uint32(fat_archs[i].offset);
+		fat_archs[i].size       = swap_uint32(fat_archs[i].size);
+		fat_archs[i].align      = swap_uint32(fat_archs[i].align);
+		i++;
+	}
+}
+
