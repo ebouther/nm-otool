f585f3f7bb9b11c5662841bd334d2d241ad67dae
ebouther <ebouther@student.42.fr>
2017-08-31 04:03:37 +0200

    nm ok with no args

--- src/nm.c
+++ src/nm.c
@@ -4,16 +4,24 @@ int main(int argc, char **argv)
 {
 	int 		fd;
 	int			i;
+	char		*file;
 
 	i = 0;
-	while (++i < argc)
+	while (++i < argc || argc == 1)
 	{
-		if ((fd = open(argv[i], O_RDONLY)) < 0)
+		if (argc == 1)
 		{
-			disp_err(argv[i], "Permission denied.\n");
+			file = ft_strcpy(ft_strnew(5), "a.out");
+			argc = 0;
+		}
+		else
+			file = argv[i];
+		if ((fd = open(file, O_RDONLY)) < 0)
+		{
+			disp_err(file, "Permission denied.\n");
 			continue ;
 		}
-		nm_otool(fd, argv[i], argc > 2 ? 1 : 0, NM);
+		nm_otool(fd, file, argc > 2 ? 1 : 0, NM);
 
 		if (close(fd) == -1)
 			continue;
