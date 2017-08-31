e9b7293cfe66c2a383fe06d9c1d2a2538ca35047
ebouther <ebouther@student.42.fr>
2017-08-31 14:43:43 +0200

    otool

---
+++ inc/otool.h
@@ -0,0 +1,11 @@
+#ifndef OTOOL_H
+# define OTOOL_H
+
+# include "nm_otool.h"
+# include "utils.h"
+# include "masks.h"
+
+# include <fcntl.h>
+# include <unistd.h>
+
+#endif
---
+++ src/otool.c
@@ -0,0 +1,22 @@
+#include "otool.h"
+
+int main(int argc, char **argv)
+{
+	int 		fd;
+	int			i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		if ((fd = open(argv[i], O_RDONLY)) < 0)
+		{
+			disp_err(argv[i], "Permission denied.\n");
+			continue ;
+		}
+		nm_otool(fd, argv[i], argc > 2 ? 1 : 0, OTOOL);
+
+		if (close(fd) == -1)
+			continue;
+	}
+	return (0);
+}
